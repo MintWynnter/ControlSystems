22b3a0d930ea9d2ad09b43944af7b8a0560985cc
Sean <sosull@udel.edu>
2022-10-28 16:00:58 -0400

    fixed some bootstrap issues, I think

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import {Button} from "react-bootstrap";
+import {routh_table} from "./functions/functions"
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      
+      <Button onClick={() => routh_table({coefs: [1, 1, 2, 8]})}>routh</Button>
     </div>
   );
 }
