cba43da3165e2c15c22042150afb6d5b66b95d0a
Sean <sosull@udel.edu>
2022-10-28 16:21:06 -0400

    actually fixed bootstraps, fixed some of routh

--- src/functions/functions.tsx
+++ src/functions/functions.tsx
@@ -6,10 +6,12 @@ import {determinant} from "./matrix_ops"
 
 export function routh_table(cheq: func): number[][]{
     const m: number = cheq.coefs.length;
-    let tab: number[][] = Array(Math.ceil(m/2)).fill(Array(m).fill(0));
+    let tab: number[][] = Array(m).fill(Array(Math.ceil(m/2)).fill(0));
     console.log(tab);
+
     tab = tab.slice(2).map((row: number[], i: number): number[] => {
         return row.slice(0, -1).map((x: number, j: number): number =>{
+            console.log(-1 * (determinant({m: [[tab[i][0], tab[i][j]], [tab[i+1][0], tab[i+1][j]]]}) / tab[i+1][0]));
             return -1 * (determinant({m: [[tab[i][0], tab[i][j]], [tab[i+1][0], tab[i+1][j]]]}) / tab[i+1][0]);
         });
     });
