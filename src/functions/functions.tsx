4c7f984d62670aff7a484c53ec0dba4f3c46966a
Sean <sosull@udel.edu>
2022-10-28 16:47:24 -0400

    it's all bad

--- src/functions/functions.tsx
+++ src/functions/functions.tsx
@@ -6,10 +6,10 @@ import {determinant} from "./matrix_ops"
 
 export function routh_table(cheq: func): number[][]{
     const m: number = cheq.coefs.length;
-    let tab: number[][] = Array(m).fill(Array(Math.ceil(m/2)).fill(0));
+    let tab: number[][] = Array(m).fill(Array(Math.ceil(m/2)).fill(0)); //this is bungus, makes the same array, not different ones
     console.log(tab);
-
-    tab = tab.slice(2).map((row: number[], i: number): number[] => {
+    tab[0][0] = 1; tab[0][1] = 2; tab[1][0] = 1; tab[1][1] = 8; //can't even hard code this nonsense
+    tab = tab.slice(2).map((row: number[], i: number): number[] => { //this just cuts the first two rows, so that's a problem
         return row.slice(0, -1).map((x: number, j: number): number =>{
             console.log(-1 * (determinant({m: [[tab[i][0], tab[i][j]], [tab[i+1][0], tab[i+1][j]]]}) / tab[i+1][0]));
             return -1 * (determinant({m: [[tab[i][0], tab[i][j]], [tab[i+1][0], tab[i+1][j]]]}) / tab[i+1][0]);
