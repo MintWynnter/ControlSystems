37a0fe883e767d4fc45863beeaf0e7d011f4c773
Sean <sosull@udel.edu>
2022-11-01 14:41:27 -0400

    routh works

--- src/functions/functions.tsx
+++ src/functions/functions.tsx
@@ -1,3 +1,5 @@
+import { copyFileSync } from "fs";
+import { NumericLiteral } from "typescript";
 import {func} from "../interfaces/func"
 import {matrix} from "../interfaces/matrix"
 import {determinant} from "./matrix_ops"
@@ -6,15 +8,24 @@ import {determinant} from "./matrix_ops"
 
 export function routh_table(cheq: func): number[][]{
     const m: number = cheq.coefs.length;
-    let tab: number[][] = Array(m).fill(Array(Math.ceil(m/2)).fill(0)); //this is bungus, makes the same array, not different ones
-    console.log(tab);
-    tab[0][0] = 1; tab[0][1] = 2; tab[1][0] = 1; tab[1][1] = 8; //can't even hard code this nonsense
-    tab = tab.slice(2).map((row: number[], i: number): number[] => { //this just cuts the first two rows, so that's a problem
-        return row.slice(0, -1).map((x: number, j: number): number =>{
-            console.log(-1 * (determinant({m: [[tab[i][0], tab[i][j]], [tab[i+1][0], tab[i+1][j]]]}) / tab[i+1][0]));
-            return -1 * (determinant({m: [[tab[i][0], tab[i][j]], [tab[i+1][0], tab[i+1][j]]]}) / tab[i+1][0]);
-        });
-    });
+    let tab: number[][] = [];
+    tab = [...tab, cheq.coefs.filter((x: number, i: number): boolean => i%2 === 0)];
+    tab = [...tab, cheq.coefs.filter((x: number, i: number): boolean => i%2 === 1)];
+    for(let i: number = 2; i < m; i++){
+        let r: number[] = [];
+        for(let j: number = 0; j < Math.ceil(m/2); j++){
+            r = [...r, -1 * (determinant({m: [[tab[i-2][0], tab[i-2][j+1]], [tab[i-1][0], tab[i-1][j+1]]]}) / tab[i-1][0]) || 0];
+        }
+        tab = [...tab, r];
+    }
+    //console.log(tab);
+    /*tab = tab.map((row: number[], i: number): number[] => {
+        if(i === 0 || i === 1){return row;}
+        else{return row.slice(0, -1).map((x: number, j: number): number =>{
+            //console.log(-1 * (determinant({m: [[tab[i-2][0], tab[i-2][j]], [tab[i-1][0], tab[i-1][j]]]}) / tab[i-1][0]));
+            return (-1 * (determinant({m: [[tab[i-2][0], tab[i-2][j+1]], [tab[i-1][0], tab[i-1][j+1]]]}) / tab[i-1][0]) || 0);
+        })}
+    });*/
     console.log(tab);
     return tab;
 }
